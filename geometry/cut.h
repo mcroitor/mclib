771518272b15ada188fd4423f5d93b2efa21a6bc
krey <krey@DESKTOP-OF4VB5S>
2017-08-08 23:05:12 +0300

    biginteger
    Conflicts:
    	nbproject/Makefile-Debug.mk
    	nbproject/Makefile-Release.mk
    	nbproject/configurations.xml

--- geometry/cut.h
+++ geometry/cut.h
@@ -3,38 +3,47 @@
 
 #include "point.h"
 
-namespace mc{
-	/*
-	* cuts
-	*/
-	template<size_type DIMENSION>
-	struct cut{
-		typedef point<DIMENSION> point_type;
-		point_type a, b;
+namespace mc {
+    namespace geometry {
 
-		cut(): a(point_type()), b(point_type()) {}
-		cut(const point_type& p1, const point_type& p2): a(p1), b(p2) {}
-		cut(const cut<DIMENSION>& p): a(p.a), b(p.b) {}
-		cut operator = (const cut<DIMENSION>& p){
-			a = p.a;
-			b = p.b;
-			return *this;
-		}
-	};
+        /*
+         * cuts
+         */
+        template<size_t DIMENSION>
+        struct cut {
+            typedef point<DIMENSION> point_type;
+            point_type a, b;
 
-	template<size_type DIMENSION>
-	bool operator == (const cut<DIMENSION>& a, const cut<DIMENSION>& b) {
-		return (a.a == b.a) && (a.b == b.b);
-	}
+            cut() : a(point_type()), b(point_type()) {
+            }
 
-	template<size_type DIMENSION>
-	std::ostream& operator << (std::ostream& os, const cut<DIMENSION>& p){
-		os << "<" << p.a << ", " << p.b << ">";
-		return os;
-	}
+            cut(const point_type& p1, const point_type& p2) : a(p1), b(p2) {
+            }
 
-	namespace g2d{
-		typedef struct cut<2> cut2d;
-	}
+            cut(const cut<DIMENSION>& p) : a(p.a), b(p.b) {
+            }
+
+            cut operator=(const cut<DIMENSION>& p) {
+                a = p.a;
+                b = p.b;
+                return *this;
+            }
+        };
+
+        template<size_t DIMENSION>
+        bool operator==(const cut<DIMENSION>& a, const cut<DIMENSION>& b) {
+            return (a.a == b.a) && (a.b == b.b);
+        }
+
+        template<size_t DIMENSION>
+        std::ostream& operator<<(std::ostream& os, const cut<DIMENSION>& p) {
+            os << "<" << p.a << ", " << p.b << ">";
+            return os;
+        }
+
+        namespace g2d {
+            typedef struct cut<2> cut2d;
+        }
+    }
 }
 #endif
