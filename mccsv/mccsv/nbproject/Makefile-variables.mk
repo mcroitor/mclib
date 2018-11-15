ae73cd5aec8f9f8bbb3a40e81470bcc8cca9b432
Михаил Кройтор <m_croitor@hotmail.com>
2018-11-15 16:33:11 +0200

    move readme.md

--- mccsv/mccsv/nbproject/Makefile-variables.mk
+++ mccsv/mccsv/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=MinGW64-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW64-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=libmccsv.a
-CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW64-Windows/libmccsv.a
-CND_PACKAGE_DIR_Debug=dist/Debug/MinGW64-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/libmccsv.a
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=mccsv.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW64-Windows/package/mccsv.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/mccsv.tar
 # Release configuration
-CND_PLATFORM_Release=MinGW64-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/MinGW64-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=libmccsv.dll
-CND_ARTIFACT_PATH_Release=dist/Release/MinGW64-Windows/libmccsv.dll
-CND_PACKAGE_DIR_Release=dist/Release/MinGW64-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/libmccsv.dll
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=libmccsv.dll.tar
-CND_PACKAGE_PATH_Release=dist/Release/MinGW64-Windows/package/libmccsv.dll.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/libmccsv.dll.tar
 #
 # include compiler specific variables
 #
