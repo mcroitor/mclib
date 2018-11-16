88795b6422e96bd7445836943cf8619774018ae3
Михаил Кройтор <m_croitor@hotmail.com>
2018-11-16 17:09:53 +0200

    catch tests

--- geometry/geometry/docs/tests/newsimpletest.cpp
+++ geometry/geometry/docs/tests/newsimpletest.cpp
@@ -10,3 +10,11 @@ TEST_CASE( "points can be created", "[point]" ) {
         REQUIRE( point.x(index) == 0 );
     }
 }
+
+TEST_CASE( "points can be created", "[point]" ) {
+    mc::geometry::point<5> point = {1, 2, 3, 4, 5};
+    REQUIRE( point.DIMENSION == 5 );
+    for(size_t index = 0; index != point.DIMENSION; ++index){
+        REQUIRE( point.x(index) == index + 1 );
+    }
+}
