3e77dc48bb06d42e4e701571015f16b78d5ab1a6
Михаил Кройтор <m_croitor@hotmail.com>
2018-11-17 09:37:48 +0200

    line primitive improvement

--- geometry/geometry/line.h
+++ geometry/geometry/line.h
@@ -18,8 +18,8 @@ namespace mc {
 
         template<size_t _DIMENSION>
         class line {
-            point<_DIMENSION> p;
-            vector<_DIMENSION> a;
+            point<_DIMENSION> _p;
+            vector<_DIMENSION> _a;
         public:
             typedef mc::geometry::point<_DIMENSION> POINT_TYPE;
 
@@ -27,32 +27,47 @@ namespace mc {
                 DIMENSION = _DIMENSION
             };
 
-            line(const point<_DIMENSION>& p1, const point<_DIMENSION>& p2) : p(p1), a(normalize(vector<_DIMENSION>(p1, p2))) {
+            line(const point<_DIMENSION>& p1, const point<_DIMENSION>& p2) : _p(p1), _a(normalize(vector<_DIMENSION>(p1, p2))) {
             }
 
-            line(const cut<_DIMENSION>& c) : p(c.a), a(normalize(vector<_DIMENSION>(c.a, c.b))) {
+            line(const cut<_DIMENSION>& c) : _p(c.a), _a(normalize(vector<_DIMENSION>(c.a, c.b))) {
             }
 
-            line(const line<_DIMENSION>& l) : p(l.p), a(l.a) {
+            line(const line<_DIMENSION>& l) : _p(l._p), _a(l._a) {
             }
 
             ~line() {
             }
 
             line<_DIMENSION> operator=(const line<_DIMENSION>& l) {
-                p = l.p;
-                a = l.a;
+                _p = l._p;
+                _a = l._a;
 
                 return *this;
             }
 
+            const vector<_DIMENSION>& a() const {
+                return _a;
+            }
+
+            const point<_DIMENSION>& p() const {
+                return _p;
+            }
+
+            bool contains(const point<_DIMENSION>& p) const {
+                vector<_DIMENSION> v(_p, p);
+                return v || _a;
+            }
             // #TODO: add to_string
         };
 
         template<size_t _DIMENSION>
         POS_TYPE operator==(const line<_DIMENSION>& l1, const line<_DIMENSION>& l2) {
-            if (!(l1.a == l2.a) && !(l1.a == -l2.a)) {
+            if (!(l1.a() || l2.a())) {
                 return INTERSECT;
+            }
+            if ((l1.a() || l2.a()) && l1.contains(l2.p())) {
+                return PARALLEL;
             }
             return MATCH;
         }
